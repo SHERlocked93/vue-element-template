c3e0439533aab2946999d709f8156a8848e863a9
SHERlocked93 <443033099@qq.com>
2018-03-02 10:38:50 +0800

    jenkins测试1

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"https://easy-mock.com/mock/59e6f5dd34be4b482ca23abe/pdspa"'    // easy-mock
+  BASE_API: '"https://easy-mock.com/mock/59e6f5dd34be4b482ca23abe/pdspa"'     // easy-mock
 }
