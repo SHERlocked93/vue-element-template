1fdd3d4ee2cb53f182ae15cfee63f92ec42c0743
SHERlocked93 <443033099@qq.com>
2018-03-08 10:41:35 +0800

    修复图片打包的路径/static/img/...前面多了个/static/css问题

--- build/utils.js
+++ build/utils.js
@@ -4,35 +4,35 @@ const config = require('../config')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const packageConfig = require('../package.json')
 
-exports.assetsPath = function (_path) {
+exports.assetsPath = function(_path) {
   const assetsSubDirectory = process.env.NODE_ENV === 'production'
-    ? config.build.assetsSubDirectory
-    : config.dev.assetsSubDirectory
-
+                             ? config.build.assetsSubDirectory
+                             : config.dev.assetsSubDirectory
+  
   return path.posix.join(assetsSubDirectory, _path)
 }
 
-exports.cssLoaders = function (options) {
+exports.cssLoaders = function(options) {
   options = options || {}
-
+  
   const cssLoader = {
     loader: 'css-loader',
     options: {
       sourceMap: options.sourceMap
     }
   }
-
+  
   const postcssLoader = {
     loader: 'postcss-loader',
     options: {
       sourceMap: options.sourceMap
     }
   }
-
+  
   // generate loader string to be used with extract text plugin
-  function generateLoaders (loader, loaderOptions) {
+  function generateLoaders(loader, loaderOptions) {
     const loaders = options.usePostCSS ? [cssLoader, postcssLoader] : [cssLoader]
-
+    
     if (loader) {
       loaders.push({
         loader: loader + '-loader',
@@ -41,11 +41,12 @@ exports.cssLoaders = function (options) {
         })
       })
     }
-
+    
     // Extract CSS when that option is specified
     // (which is the case during production build)
     if (options.extract) {
       return ExtractTextPlugin.extract({
+        publicPath: '../../',
         use: loaders,
         fallback: 'vue-style-loader'
       })
@@ -53,7 +54,7 @@ exports.cssLoaders = function (options) {
       return ['vue-style-loader'].concat(loaders)
     }
   }
-
+  
   // https://vue-loader.vuejs.org/en/configurations/extract-css.html
   return {
     css: generateLoaders(),
@@ -67,10 +68,10 @@ exports.cssLoaders = function (options) {
 }
 
 // Generate loaders for standalone style files (outside of .vue)
-exports.styleLoaders = function (options) {
+exports.styleLoaders = function(options) {
   const output = []
   const loaders = exports.cssLoaders(options)
-
+  
   for (const extension in loaders) {
     const loader = loaders[extension]
     output.push({
@@ -78,19 +79,19 @@ exports.styleLoaders = function (options) {
       use: loader
     })
   }
-
+  
   return output
 }
 
 exports.createNotifierCallback = () => {
   const notifier = require('node-notifier')
-
+  
   return (severity, errors) => {
     if (severity !== 'error') return
-
+    
     const error = errors[0]
     const filename = error.file && error.file.split('!').pop()
-
+    
     notifier.notify({
       title: packageConfig.name,
       message: severity + ': ' + error.name,
