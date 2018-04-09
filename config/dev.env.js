72ed343ed920a45c6d3e6436360abca07bf90859
SHERlocked93 <443033099@qq.com>
2018-04-09 20:01:27 +0800

    更改地址

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"https://easy-mock.com/mock/59e6f5dd34be4b482ca23abe/pdspa"'    // easy-mock
+  BASE_API: '"https://easy-mock.com/mock/59e6f5dd34be4b482ca23abe/ele-template"'    // easy-mock
 })
