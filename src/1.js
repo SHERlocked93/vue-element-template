631a6862f8bea8b1f2cfd6bbfa0c62141e74cb17
SHERlocked93 <443033099@qq.com>
2018-04-11 21:21:45 +0800

    引入flowjs

---
+++ src/1.js
@@ -0,0 +1,8 @@
+// @flow
+export const num: number = 12
+export const str: string = 'a'
+export const str2: string = 'abc'
+export const b: boolean = true
+export const b1: boolean = false
+export const n: null = null
+export const undef: void = undefined
--- src/main.js
+++ src/main.js
@@ -2,20 +2,20 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
-import store from './store'                           // noinspection ES6CheckImport
+import store from './store' // noinspection ES6CheckImport
 import * as Filters from 'utils/filters'
 
-import '@/permission'                                 // 路由守卫处理权限
-import 'normalize.css/normalize.css'                  // 浏览器样式初始化
-import 'element-ui/lib/theme-chalk/index.css'         // element 样式
-import '@/styles/index.scss'                          // style下自定义样式
-import 'assets/icon/iconfont.css'                     // iconfont导出的图标
+import '@/permission' // 路由守卫处理权限
+import 'normalize.css/normalize.css' // 浏览器样式初始化
+import 'element-ui/lib/theme-chalk/index.css' // element 样式
+import '@/styles/index.scss' // style下自定义样式
+import 'assets/icon/iconfont.css' // iconfont导出的图标
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI, { size: 'medium' })
 
-Object.keys(Filters).forEach(T => Vue.filter(T, Filters[T]))      // VUE注册全局filters
+Object.keys(Filters).forEach(T => Vue.filter(T, Filters[T])) // VUE注册全局filters
 
 new Vue({
   el: '#app',
